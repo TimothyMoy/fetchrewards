31b825517e5a58dc8759676631ec294e2dc1eb28
TimothyMoy <Timothy.z.moy@gmail.com>
2020-09-21 10:30:52 -0700

    created app class to extend component

--- fetchapp/src/App.js
+++ fetchapp/src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-  
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        Hello
+      </div>
+    );
+  }
 }
 
 export default App;
