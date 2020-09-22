05632459a5a3f51bbfb90f8a0968a879e3fd7618
TimothyMoy <Timothy.z.moy@gmail.com>
2020-09-22 10:07:39 -0700

    updated notes and readme

---
+++ README.md
@@ -0,0 +1,12 @@
+Fetch Reward Coding Exercise
+
+The goal of this exercise is to write a web app that retrieve data from a json file.
+
+The web app should do the following task:
+
+Display all the items grouped by "listId"
+Sort the results first by "listId" then by "name" when displaying.
+Filter out any items where "name" is blank or null.
+The final result should be displayed to the user in an easy-to-read list.
+
+For this project I mainly used React.
--- fetchapp/src/components/ItemList.js
+++ fetchapp/src/components/ItemList.js
@@ -2,9 +2,12 @@ import React from 'react';
 import Item from './Item';
 
 function ItemList (props) {
-  console.log('Item List = ', props);
+  // Checking props to target specific items in object
+  // console.log('Item List = ', props);
 
+  // Map out the items object
   let itemList = props.items.map((itemObj) => {
+    // filter out item objects with the name of null and blank
     if (itemObj.name !== null && itemObj.name !== "") {
       return(
         <Item
@@ -14,9 +17,12 @@ function ItemList (props) {
         )
       }
     })
+    // sort item id first because it's the same as name 
     itemList.sort((a,b) => a.props.item.id - b.props.item.id)
+    // sort list id second because there's four specific grouped values
     itemList.sort((a,b) => a.props.item.listId - b.props.item.listId)
 
+    // Error handling incase no items found
     if (props.items.length === 0) {
       itemList = <p>No Items found</p>
     }
